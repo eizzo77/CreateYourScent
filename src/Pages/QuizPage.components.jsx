1b4c711a880049cc59caf854d542a5520534c32d
Itay <eizzo77@gmail.com>
2021-05-11 23:58:04 +0300

    added 2 sections

--- src/Pages/QuizPage.components.jsx
+++ src/Pages/QuizPage.components.jsx
@@ -1,19 +1,21 @@
 import react from "react";
 import {JarSelection} from "../components/JarSelection.components";
+import { ScentSelection } from "../components/ScentSelection.components";
 import {WaxSelection} from "../components/WaxSelection.components";
 import "./QuizPage.components.css"
 
-const quizLevels = [<JarSelection title="Pick The Type of Jar" />,<WaxSelection title="Choose your favorite Wax"/> ]
+const quizsections = [  <JarSelection title="Pick The Type of Jar" />,
+                        <WaxSelection title="Choose your favorite Wax"/>,
+                        <ScentSelection title="What are your favorite Scents?"/>]
 
 export const QuizPage = () => {
     return(
         <div className="quiz-page">
             <div className="quiz-container">
-                {quizLevels[1]}
+                {quizsections[2]}
             </div>
-            <div className="selections-nav">
-
-            </div>
+            {/* <div className="selections-nav">
+            </div> */}
         </div>
     );
 }
--- src/components/WaxSelection.components.jsx
+++ src/components/WaxSelection.components.jsx
@@ -1,11 +1,70 @@
-
+import "./WaxSelection.components.css"
 
 export const WaxSelection = ({title}) => {
     return(
         <>
             <h3 className="level-title">
                 {title}
-                </h3>
-        </>
+            </h3>
+<div id="hexcontainer">
+  <div class="hex1">
+    <div class="hex2">
+      <a class="hexlink" id="hl1" href="">
+        <div class="hexcover"></div>
+        <h3>Pure BeeWax</h3>
+        <p>description about the this wax</p>
+        <div class="plus"></div>
+      </a>
+    </div>
+  </div>
+  <div class="hex1">
+    <div class="hex2">
+      <a class="hexlink" id="hl2" href="">
+        <div class="hexcover"></div>
+        <h3>White BeeWax</h3>
+        <p>description about the this wax</p>
+        <div class="plus"></div>
+      </a>
+    </div>
+  </div>
+  <div class="hex1">
+    <div class="hex2">
+      <a class="hexlink" id="hl3" href="">
+        <div class="hexcover"></div>
+        <h3>Soy Vegan Wax</h3>
+        <p>description about the this wax</p>
+        <div class="plus"></div>
+      </a>
+    </div>
+  </div>
+
+  <br />
+
+  <div class="hex1">
+    <div class="hex2">
+      <a class="hexlink" id="hl4" href="">
+        <div class="hexcover"></div>
+        <h3>Massage Coconut Pure Wax</h3>
+        <p>description about the this wax</p>
+        <div class="plus"></div>
+      </a>
+    </div>
+  </div>
+  <div class="hex1">
+    <div class="hex2">
+      <a class="hexlink" id="hl5" href="">
+        <div class="hexcover"></div>
+        <h3>Chia Seeds Oil</h3>
+        <p>description about the this wax</p>
+        <div class="plus"></div>
+      </a>
+    </div>
+  </div>
+
+
+
+  
+</div>  
+    </>
     );
 } 
